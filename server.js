3ee575ecadc7a3bbda173ee19d530cc1f0d7fda1
Patrick Gustafson <ptgt1999@gmail.com>
2024-07-30 23:24:12 -0400

    removed unecessary word

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(routes);
 
 app.listen(PORT, (error) => {
     if (error) {
-        console.console.error("Error starting the server: ", error);
+        console.error("Error starting the server: ", error);
     } else {
         console.log(`Server is listening on ${PORT}`);
     }
